7f000032651c72195c1c3a24e592c2e4c0020373
Kirti <kashyap.kirti11@hmail.com>
2020-06-27 20:28:28 +0100

    unit test for get screen size

--- app/src/androidTest/java/com/wiprotest/demo/MainActivityTest.kt
+++ app/src/androidTest/java/com/wiprotest/demo/MainActivityTest.kt
@@ -1,5 +1,6 @@
 package com.wiprotest.demo
 
+import android.content.res.Configuration
 import android.view.View
 import androidx.test.runner.AndroidJUnit4
 //import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -14,18 +15,44 @@ import org.junit.runners.JUnit4
 /* Unit test for activity successfully launched or not */
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
-    /*for activity test we need activity rule*/
+    /* for activity test we need activity rule */
     @get:Rule
     val mActivityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
-    lateinit var mManiActivity:  MainActivity
+    private lateinit var mManiActivity:  MainActivity
         @Before
         fun setUp() {
             /*get the instance for activity*/
             mManiActivity = mActivityRule.activity
         }
+    @After
+    fun tearDown() {
+
+    }
     @Test
     fun launch(){
-        val view : View =mManiActivity.linearLayout
-        assertNotNull(view) // if view is not null the activity is successfully launched
+        val view : View = mManiActivity.linearLayout
+        assertNotNull(view) /* if view is not null the activity is successfully launched */
+    }
+
+    @Test
+    fun testScreen_SIZE_SMALL() {
+        val screenSize = mManiActivity.getScreenSize(1)
+        assertEquals("small", screenSize) /* if screen is small */
+    }
+
+    @Test
+    fun testScreenSize_SIZE_MEDIUM() {
+        val screenSize = mManiActivity.getScreenSize(2)
+        assertEquals("medium",screenSize) /* if screen is medium */
+    }
+    @Test
+    fun testScreenSize_SIZE_LARGE() {
+        val screenSize = mManiActivity.getScreenSize(3)
+        assertEquals("large",screenSize) /* if screen is large */
+    }
+    @Test
+    fun testScreenSize_SIZE_XTRALARGE() {
+        val screenSize = mManiActivity.getScreenSize(4)
+        assertEquals("extralarge",screenSize) /* if is extra large */
     }
 }
---
+++ app/src/androidTest/java/com/wiprotest/demo/ui/DetailActivityTest.kt
@@ -0,0 +1,17 @@
+package com.wiprotest.demo.ui
+
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class DetailActivityTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
