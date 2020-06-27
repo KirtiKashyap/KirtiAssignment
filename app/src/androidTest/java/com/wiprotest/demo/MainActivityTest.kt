64c608d4e785019bffde41c9305f883c6717867c
Kirti <kashyap.kirti11@hmail.com>
2020-06-27 18:14:30 +0100

    activity launch test

--- app/src/androidTest/java/com/wiprotest/demo/MainActivityTest.kt
+++ app/src/androidTest/java/com/wiprotest/demo/MainActivityTest.kt
@@ -11,27 +11,21 @@ import org.junit.Assert.*
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
-/* Unit test for activity successfully launced or not */
+/* Unit test for activity successfully launched or not */
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
-    /*for activity test we need activirt rule*/
+    /*for activity test we need activity rule*/
     @get:Rule
     val mActivityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
     lateinit var mManiActivity:  MainActivity
-//    companion object {
         @Before
         fun setUp() {
             /*get the instance for activity*/
             mManiActivity = mActivityRule.activity
         }
-//    }
-@Test
-fun launch(){
-    val view : View =mManiActivity.linearLayout
-    assertNotNull(view) // if view is not null the activity is successfully launched
-}
-    @After
-    fun tearDown() {
-
+    @Test
+    fun launch(){
+        val view : View =mManiActivity.linearLayout
+        assertNotNull(view) // if view is not null the activity is successfully launched
     }
 }
